b24f9b83600be3de78e90f8aa0056a92fd428cc9
ZukisaNante <zukisanante@gmail.com>
2020-03-26 18:53:09 +0100

    new server component created

---
+++ myWebsite/src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // pass javascript in this component
+selector: 'app-server',
+templateUrl: './server.component.html'
+})
+// make it usable all over the web(outside)
+export class ServerComponent {
+
+}
